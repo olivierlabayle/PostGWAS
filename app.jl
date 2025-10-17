64fac02321dcc05a4869c2d5fed01762afefcea1
Olivier Labayle <olabayle@gmail.com>
2025-10-17 10:59:59 +0100

    update app

--- app.jl
+++ app.jl
@@ -13,45 +13,75 @@ global loci_ids = collect(keys(loci_dataset))
 
 @app begin
     @in selected_locus = first(loci_ids)
-
-    @out positions = []
-    @out log10pvalues = []
     
     @out traces = []
-    @out plot_layout = PlotlyJS.Layout()
+    @out layout = PlotlyJS.Layout()
 
     @onchange selected_locus begin
       println(selected_locus)
       locus = loci_dataset[selected_locus]
+      println("ok 0")
       positions = locus.POS
       log10pvalues = locus.LOG10P
-      traces = [scatter(
-        x=positions,
-        y=log10pvalues,
-        mode="markers",
-        text=locus.ID,
-        name="Trace 1"
-      )
-      scatter(
-        x=positions,
-        y=locus.PIP,
-        mode="markers",
-        name="Trace 2"
-      )
-      ]
-      plot_layout = PlotlyJS.Layout(
-        title=string("Locus View: ", selected_locus),
-        xaxis=attr(
-            title="Position",
-            showgrid=false
+      pips = locus.PIP
+      variant_ids = locus.ID
+      ld = locus.PHASED_R2
+      cs = locus.CS
+      println(unique(locus.CS))
+      println("ok 1")
+
+      subplots = make_subplots(rows=2, cols=1, shared_xaxes=true, vertical_spacing=0.02)
+      add_trace!(subplots,
+          scatter(
+          x=positions,
+          y=log10pvalues,
+          mode="markers",
+          text=variant_ids,
+          showlegend=false,
+          marker = attr(
+            color = ld,
+            colorscale = "Viridis",
+            showscale = true,
+            colorbar = attr(
+                title = "LD",
+                y = 0.5,     # position higher up
+                len = 0.5,   # roughly half the figure height
+                yanchor = "bottom"
+            ))
         ),
-        yaxis=attr(
-            title="-log10(p-value)",
-            showgrid=true,
-            range=[0, 20]
-        )
+        row=1,
+        col=1
       )
-      println("ok")
+      add_trace!(subplots,
+        scatter(
+          x=positions,
+          y=pips,
+          showlegend=true,
+          mode="markers",
+          text=variant_ids,
+          marker = attr(color = cs),
+          name = "CS"
+        ),
+        row=2,
+        col=1
+      )
+      relayout!(subplots; 
+        xaxis2_title="Position",
+        yaxis_title="-log₁₀(p-value)",
+        yaxis2_title="PIP",
+        show_legend=true,
+        legend = attr(
+          y = -0.1,  # centered below plot
+          xanchor = "center",
+          yanchor = "top"
+      )
+      )
+
+      # shared_xaxes=true, vertical_spacing=0.02,
+      # relayout!(subplots, showlegend=false, title_text=string("LocusZoom ",  selected_locus))
+      println("ok 3")
+      traces = subplots.plot.data
+      layout = subplots.plot.layout
     end
     # Reactive plots interactions
     @mixin traces::PlotlyEvents
@@ -85,7 +115,7 @@ function ui()
       )
     ])
     cell([
-      StipplePlotly.plot(:traces, layout=:plot_layout, syncevents=true)
+      StipplePlotly.plot(:traces, layout=:layout, syncevents=true)
     ])
     ]
 end
